83a6401f0ad520bde646e54923756608177b1731
sahal9431 <sahalms9061@gmail.com>
2026-01-13 20:36:39 +0530

    New commit

--- playwrite_demo/test_playwrightPractice.py
+++ playwrite_demo/test_playwrightPractice.py
@@ -16,32 +16,68 @@ import time
 #     '''
 #     page.goto("https://www.google.com/")
 
-def test_coreLocators(playwright: Playwright):
+# def test_coreLocators(playwright: Playwright):
+#     browser = playwright.chromium.launch(headless=False)
+#     #browser = playwright.firefox.launch(headless=False)
+#     browser_context = browser.new_context()
+#     page = browser_context.new_page()
+#     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+#     username = page.get_by_label("Username").fill("rahulshettyacademy")
+#     password = page.get_by_label("Password").fill("learning")
+#     page.get_by_role("combobox").select_option("teach")
+#     time.sleep(5)
+#     page.locator("#terms").check()
+#     #page.locator("text=Sign In").click()  alternative way to click                         
+#     # for cssselector if there is id #then id  for class .then class
+#     #page.get_by_role("link", name="Terms & Conditions").click()
+#     page.get_by_role("button", name="Sign In").click()
+#     #expect(page.get_by_text("Incorrect")).to_be_visible()
+#     #expect(page.locator(".alert-danger")).to_contain_text("Incorrect")
+#     iphoneProduct = page.locator("app-card").filter(has_text="iphone X")
+#     iphoneProduct.locator("text=Add").click()
+#     nokiaProduct = page.locator("app-card").filter(has_text="Nokia Edge")
+#     nokiaProduct.locator("text=Add").click()
+#     time.sleep(5)
+#     checkoutButton = page.get_by_text("Checkout").click()
+#     time.sleep(5)
+#     expect(page.locator(".media-body")).to_have_count(2)
+
+# def test_childPageTest(playwright: Playwright):
+#     browser = playwright.chromium.launch(headless=False)
+#     browser_context = browser.new_context()
+#     page = browser_context.new_page()
+#     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+#     with page.expect_popup() as new_page_info:
+#         page.locator(".blinkingText").click()
+#         time.sleep(5)
+#         child_page = new_page_info.value
+#         text = child_page.locator(".red").text_content()
+#         print(text)
+#         word = text.split("at")
+#         Email = word[1].strip().split(" ")[0]
+#         assert Email == "mentor@rahulshettyacademy.com"
+#         assert "mentor@rahulshettyacademy.com" in text
+
+def test_ui_Validation(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
-    #browser = playwright.firefox.launch(headless=False)
     browser_context = browser.new_context()
     page = browser_context.new_page()
-    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
-    username = page.get_by_label("Username").fill("rahulshettyacademy")
-    password = page.get_by_label("Password").fill("learning")
-    page.get_by_role("combobox").select_option("teach")
-    time.sleep(5)
-    page.locator("#terms").check()
-    #page.locator("text=Sign In").click()  alternative way to click                         
-    # for cssselector if there is id #then id  for class .then class
-    #page.get_by_role("link", name="Terms & Conditions").click()
-    page.get_by_role("button", name="Sign In").click()
-    #expect(page.get_by_text("Incorrect")).to_be_visible()
-    #expect(page.locator(".alert-danger")).to_contain_text("Incorrect")
-    iphoneProduct = page.locator("app-card").filter(has_text="iphone X")
-    iphoneProduct.locator("text=Add").click()
-    nokiaProduct = page.locator("app-card").filter(has_text="Nokia Edge")
-    nokiaProduct.locator("text=Add").click()
-    time.sleep(5)
+    page.goto("https://rahulshettyacademy.com/AutomationPractice/")
 
+    page.get_by_placeholder("Hide/Show Example").is_visible()
+    page.get_by_role("button", name="Hide").click()
+    assert page.get_by_placeholder("Hide/Show Example").is_hidden()
 
+    #Alert Handling
+    page.get_by_role("button", name="Confirm").click()
+    time.sleep(10)
+    page.on("dialog", lambda dialog: dialog.accept())
+    #lambda fuction is used to create small anonymous function at runtime in single line
 
-
+    #Frames Handling
+    pageFrame = page.frame_locator("#courses-iframe")
+    pageFrame.get_by_role("link", name="All Access plan").click()
+    expect(pageFrame.locator(".text")).to_contain_text(" Happy Subscibers!")
 
 
 
